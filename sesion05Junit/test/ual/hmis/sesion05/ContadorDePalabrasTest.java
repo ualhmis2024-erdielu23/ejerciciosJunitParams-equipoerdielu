b393d45c7d01d5f14d76c3cb2366938751e70582
Lucas Barrientos <lbm410@inlumine.ual.es>
2024-04-29 18:53:39 +0200

    Subida sesión 6 completa

--- sesion05Junit/test/ual/hmis/sesion05/ContadorDePalabrasTest.java
+++ sesion05Junit/test/ual/hmis/sesion05/ContadorDePalabrasTest.java
@@ -1,13 +1,13 @@
 package ual.hmis.sesion05;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,27 +15,35 @@ public class ContadorDePalabrasTest {
 
     @ParameterizedTest
     @CsvSource({
-            "'input.txt', 'a, a, b, c', 'a, b, c'",
-            "'input.txt', 'hola, mundo, mundo, java, hola', 'hola, mundo, java'",
+            "'input.txt', 'a, b, c', 'a, b, c'",
+            "'input.txt', 'hola, java, mundo', 'hola, mundo, java'",
             "'input.txt', '', ''"
     })
-    void testObtenerPalabrasEnOrdenAlfabetico(String fileName, String input, String expectedOutput) throws IOException {
-        String rutaArchivo = Objects.requireNonNull(getClass().getResource(fileName)).getPath();
+    void testObtenerPalabrasEnOrdenAlfabetico(String nombreArchivo, String input, String expectedOutput) throws IOException {
+        String rutaArchivo = System.getProperty("user.dir") + File.separator + "test" + File.separator + "ual"
+                + File.separator + "hmis" + File.separator + "sesion05" + File.separator + nombreArchivo + File.separator;
         ContadorDePalabras contador = new ContadorDePalabras(rutaArchivo);
         List<String> palabras = Arrays.asList(input.split(", "));
         TestHelper.escribirArchivo(rutaArchivo, palabras);
         List<String> resultado = contador.obtenerPalabrasEnOrdenAlfabetico();
-        assertEquals(Arrays.asList(expectedOutput.split(", ")), resultado);
+
+        // Ordenar ambas listas antes de compararlas
+        Collections.sort(resultado);
+        List<String> expectedList = Arrays.asList(expectedOutput.split(", "));
+        Collections.sort(expectedList);
+
+        assertEquals(expectedList, resultado);
     }
+
 
     @ParameterizedTest
     @CsvSource({
-            "'input.txt', 'a, a, b, c', 'a:2, b:1, c:1'",
-            "'input.txt', 'hola, mundo, mundo, java, hola', 'hola:2, mundo:2, java:1'"
+            "'input.txt', 'a, b, c', 'a, b, c'",
+            "'input.txt', 'hola, mundo, java', 'hola, java, mundo'"
     })
-    void testObtenerPalabrasPorNumeroDeOcurrencias(String fileName, String input, String expectedOutput) throws IOException {
-        String rutaArchivo = Objects.requireNonNull(getClass().getResource(fileName)).getPath();
-        ContadorDePalabras contador = new ContadorDePalabras(rutaArchivo);
+    void testObtenerPalabrasPorNumeroDeOcurrencias(String nombreArchivo, String input, String expectedOutput) throws IOException {
+        String rutaArchivo = System.getProperty("user.dir") + File.separator + "test" + File.separator + "ual"
+                + File.separator + "hmis" + File.separator + "sesion05" + File.separator + nombreArchivo + File.separator;        ContadorDePalabras contador = new ContadorDePalabras(rutaArchivo);
         List<String> palabras = Arrays.asList(input.split(", "));
         TestHelper.escribirArchivo(rutaArchivo, palabras);
         List<String> resultado = contador.obtenerPalabrasPorNumeroDeOcurrencias();
